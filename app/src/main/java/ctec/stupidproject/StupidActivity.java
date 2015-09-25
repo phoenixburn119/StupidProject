df1fd9f33f61a9de4df89032c038f6a6ceb81c57
Adam Kinzer <adamkinzer915@gmail.com>
2015-09-25 13:10:50 -0600

    Added a Button

--- app/src/main/java/ctec/stupidproject/StupidActivity.java
+++ app/src/main/java/ctec/stupidproject/StupidActivity.java
@@ -4,13 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
+import android.view.View;
+import android.graphics.Color;
 
-public class StupidActivity extends AppCompatActivity {
+public class StupidActivity extends AppCompatActivity
+{
+    private Button colorChangeButton;
+    private RelativeLayout background;
+    private TextView sillyWords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stupid);
+
+        colorChangeButton = (Button) findViewById(R.id.sillyButton);
+        background = (RelativeLayout) findViewById(R.id.appBackground);
+        sillyWords = (TextView) findViewById(R.id.sillyWords);
+
+        setupListeners();
     }
 
     @Override
@@ -33,5 +48,41 @@ public class StupidActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void changeColors()
+    {
+        int redColor;
+        int greenColor;
+        int blueColor;
+
+        redColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+
+        background.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
+
+        redColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+
+        colorChangeButton.setBackgroundColor(Color.rgb(redColor,greenColor, blueColor));
+
+        redColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+
+        colorChangeButton.setTextColor(Color.rgb(redColor, greenColor, blueColor));
+    }
+
+    private void setupListeners()
+    {
+        colorChangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View buttonView) {
+                //This is where you put code that happens when you click a button
+                changeColors();
+            }
+        });
     }
 }
